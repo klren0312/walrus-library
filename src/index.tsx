624f1069fd99c32e80f04b0cd4a2bdc1b8fc274d
ZCDC_Ren <renzw@zzes1314.cn>
2024-12-12 18:13:49 +0800

    feat: init

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import { Main } from './main'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/locale/zh_CN'
 
 const container = document.querySelector('#root')
 if (container) {
   const root = createRoot(container)
   root.render(
     <Router>
-      <Main />
+      <ConfigProvider locale={zhCN}>
+        <Main />
+      </ConfigProvider>
     </Router>
   )
 }
